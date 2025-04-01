4131482a6db6b9186400f5a1dd06573af289237a
Taras <taras@coderiver.com.ua>
2025-04-01 19:43:21 +0300

    added menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './scss/style.scss';
+
+const body = document.body;
+const burger = body.querySelector('.burger');
+const burgerLine = burger.querySelector('.burger__line');
+const menu = body.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--active');
+  burgerLine.classList.toggle('burger__line--active');
+  menu.classList.toggle('menu--active');
+});
