75624c11cb9a330f1e415ab247ee3e09cba9cd6e
Taras <taras@coderiver.com.ua>
2025-04-07 18:28:22 +0300

    small fixes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,39 @@
 import './scss/style.scss';
 
 const body = document.body;
-const burger = body.querySelector('.burger');
+const header = body.querySelector('.header');
+const burger = header.querySelector('.burger');
 const burgerLine = burger.querySelector('.burger__line');
 const menu = body.querySelector('.menu');
 
+let menuIsOpened = false;
+
 burger.addEventListener('click', () => {
-  burger.classList.toggle('burger--active');
-  burgerLine.classList.toggle('burger__line--active');
-  menu.classList.toggle('menu--active');
+  if (!menuIsOpened) {
+    menuIsOpened = true;
+    burger.classList.add('burger--active');
+    burgerLine.classList.add('burger__line--active');
+    menu.classList.add('menu--active');
+    body.style.overflow = 'hidden';
+
+    return;
+  }
+
+  menuIsOpened = false;
+  burger.classList.remove('burger--active');
+  burgerLine.classList.remove('burger__line--active');
+  menu.classList.remove('menu--active');
+  body.style.overflow = '';
+});
+
+window.addEventListener('scroll', () => {
+  const { scrollY } = window;
+
+  if (scrollY > 0 && !header.classList.value.includes('header--black')) {
+    header.classList.add('header--black');
+  }
+
+  if (scrollY === 0 && header.classList.value.includes('header--black')) {
+    header.classList.remove('header--black');
+  }
 });
